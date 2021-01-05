757885d0e617750e5b5cdd3416473088982ea9a0
denispolak020 <77003229+denispolak020@users.noreply.github.com>
2021-01-05 22:56:46 +0100

    Add files via upload

---
+++ program4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+#include<math.h>
+int main () {
+int number;
+int rt;
+printf("Enter number\n");
+scanf("%d",&number);
+
+rt = sqrt(number);
+printf("Square root of %d = %d",number,rt);
+
+
+
+
+return 0;
+}
